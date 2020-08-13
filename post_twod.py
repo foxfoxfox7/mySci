6d7f084f41efb7574c74d10a55ebdaf89a3df1a6
Kieran Fox <kieranfox7@gmail.com>
2020-08-13 15:15:03 +0200

    2d anisotropy in post

--- post_twod.py
+++ post_twod.py
@@ -4,10 +4,13 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-import quantarhei as qr
-
+import warnings
+warnings.filterwarnings("ignore")
 import sys
 sys.path.append('/home/kieran/Work/mySci')
+
+import quantarhei as qr
+
 import myFuncs
 
 
@@ -24,8 +27,7 @@ def displayState(func, order):
 # Loading the dictionaries
 ########################################################################
 
-save_dir = './test_new/'
-file_name = 'resp'
+save_dir = './N8_D5_SD150_T200_40_500/'
 
 las_pol = ['para', 'perp']
 resp_names = ['rGSB', 'nGSB', 'rSE', 'nSE','rESA', 'nESA', 'rSEWT', 
@@ -40,6 +42,10 @@ resp_containers_part, t2_dict = myFuncs.load_resp_dict(
     signal_names,
     test_pad = 800
     )
+
+#with open(save_dir + 'params' + '.pkl', 'rb') as f:
+#    params_dict = pickle.load(f)
+#print(params_dict.keys())
 
 ########################################################################
 # Get spectra
@@ -81,7 +87,7 @@ for m, laser in enumerate(las_pol):
             V = myFuncs.get_cross_points(twod_p, 11600, 12080)
 
             angle2.append(myFuncs.get_angle_from_points(V[0], V[1]))
-            widths.append(myFuncs.get_peak_cross_sections(V, twod_p))
+            #widths.append(myFuncs.get_peak_cross_sections(V, twod_p))
 
             #twod_p.plot()
             #plt.scatter(V[:,0], V[:,1])
@@ -93,6 +99,8 @@ print('angle')
 print(angle2)
 print('widths')
 print(widths)
+print(widths[:,0])
+print(widths[:,1])
 
 ########################################################################
 # Anisotropy (points)
@@ -131,45 +139,39 @@ if _save_:
 ########################################################################
 # Anisotropy (2d plot)
 ########################################################################
-'''
-data_for_anis = [[],[]]
-x_for_anis = [[],[]]
-y_for_anis = [[],[]]
+
+#with qr.energy_units('1/cm'):
+#    xaxis = np.array(spectra[0][0].xaxis.data[lim1:lim2])
+#    yaxis = np.array(spectra[0][0].yaxis.data[lim1:lim2])
+
+for i, tt2 in enumerate(t2_dict.data):
+    hh = np.array(spectra[0][i].data)#[lim1:lim2,lim1:lim2])
+    hv = np.array(spectra[1][i].data)#[lim1:lim2,lim1:lim2])
+    anis_array = (hh - hv)/(hh + (2 * hv))
+    print(anis_array.shape)
+    print(anis_array[500,500])
+
+    with qr.energy_units('1/cm'):
+        one_twod = qr.TwoDSpectrum()
+        one_twod.set_axis_1(spectra[0][0].xaxis)
+        one_twod.set_axis_3(spectra[0][0].yaxis)
+        one_twod.set_t2(tt2)
+        one_twod.set_data(anis_array)
+
+        print(one_twod.get_max_value())
+
+        one_twod.plot()
+        plt.xlim(en1, en2)
+        plt.ylim(en1, en2)
+        plt.show()
+
+        di_cut = one_twod.get_diagonal_cut()
+        di_cut.plot()
+        plt.show()
 
 
-for m, laser in enumerate(las_pol):
-    spec_cont_p = resp_containers_part[m].get_TwoDSpectrumContainer()
-    for i, tt2 in enumerate(t2_dict.data):
-        twod_p = spec_cont_p.get_spectrum(tt2)
-        with qr.energy_units('1/cm'):
-
-            data_for_anis[m].append(twod_p.data)
-            x_for_anis[m].append(twod_p.xaxis.data)
-            y_for_anis[m].append(twod_p.yaxis.data)
-
-data_para = np.array(data_for_anis[0])
-data_perp = np.array(data_for_anis[1])
-data_para = data_para.real
-data_perp = data_perp.real
-
-print('para')
-print(data_para)
-print('perp')
-print(data_perp)
 
 
-anis_data = (data_para - data_perp)/(data_para + (2 * data_perp))
-print('anis')
-print(anis_data[0])
-print(x_for_anis[0])
 
-fig, ax = plt.subplots(1,1)
-cp = ax.contourf(x_for_anis[0][0], y_for_anis[0][0], anis_data[0])
-plt.xlim(10000,14000)
-plt.ylim(10000,14000)
-fig.colorbar(cp) # Add a colorbar to a plot
-#ax.set_title('Filled Contours Plot')
-#ax.set_xlabel('x (cm)')
-#ax.set_ylabel('y (cm)')
-plt.show()
-'''
+
+
